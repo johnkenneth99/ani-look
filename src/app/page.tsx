eab88972a3321c9add414478d0285e6e109c1bbd
Kenneth Sinay <jksinay@gmail.com>
2024-08-10 15:58:35 +0800

    Added clsx and twMerge pckg.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/utils";
 import Image from "next/image";
 
 export default async function Home() {
@@ -19,23 +20,15 @@ export default async function Home() {
 			<div className="group flex  border-8 border-red-800 p-6 ">
 				{!!test.length &&
 					test.map((link, index) => {
-						const temp = index ? "-ml-24" : "";
-						console.log(temp);
 						return (
 							<div
 								key={link}
-								className={`${temp} relative duration-[250ms] group-hover:ml-0
-								hover:z-10
-								hover:scale-150 border-2 border-green-100 w-[150px] h-[200px]`}
+								className={cn(
+									"relative duration-[250ms] group-hover:ml-0 hover:z-10 hover:scale-150 border-2 border-green-100 w-[150px] h-[200px]",
+									{ "-ml-24": !!index }
+								)}
 							>
-								<Image
-									alt="background"
-									src={link}
-									fill
-									// width={150}
-									// height={50}
-									// style={{ objectFit: "contain" }}
-								/>
+								<Image alt="background" src={link} fill />
 							</div>
 						);
 					})}
